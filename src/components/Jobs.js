638aa35c4e236bd39cac71faaabbca55443c481d
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-18 02:48:03 -0700

    strapi query setup

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: {fields: strapiId, order: DESC}) {
+      nodes {
+        company
+        date
+        id
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+  const { allStrapiJobs: { nodes: jobs } } = data
+  const [value, setValue] = useState(0)
+
+  return <section className="sections jobs">
+    <Title title="experience" />
+    <div 
+  </section>
 }
 
 export default Jobs
