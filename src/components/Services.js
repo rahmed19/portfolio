3bcb171aeb2f88e82d812184bd846886b0c66c6b
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-18 01:22:00 -0700

    strapi setup

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,17 @@ import services from "../constants/services"
 const Services = () => {
   return <section className="section bg-grey">
     <Title title="services" />
-
+    <div className="section-center services-center">
+      {services.map((service) => {
+        const { id, icon, title, text } = service
+        return <artcile key={id} className="service">
+          {icon}
+          <h4>{title}</h4>
+          <div className="underline" />
+          <p>{text}</p>
+        </artcile>
+      })}
+    </div>
   </section>
 }
 
