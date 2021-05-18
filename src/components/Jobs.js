28399ac26729c04c540c528e0cbf1c34fb8e2ea3
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-18 02:59:56 -0700

    dynamic setup of buttons

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -8,6 +8,7 @@ const query = graphql`
   {
     allStrapiJobs(sort: {fields: strapiId, order: DESC}) {
       nodes {
+        strapiId
         company
         date
         id
@@ -25,10 +26,21 @@ const Jobs = () => {
   const data = useStaticQuery(query)
   const { allStrapiJobs: { nodes: jobs } } = data
   const [value, setValue] = useState(0)
+  const { company, position, date, desc } = jobs[value]
 
   return <section className="sections jobs">
     <Title title="experience" />
-    <div 
+    <div className="jobs-center">
+      <div className="btn-container">
+        {jobs.map((item, index) => {
+          return <button
+            key={item.strapiId}
+            className={`job-btn ${index === value && 'active-btn'}`}
+            onClick={() => setValue(index)}
+          >{item.company}</button>
+        })}
+      </div>
+    </div>
   </section>
 }
 
