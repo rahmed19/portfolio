9e0d1d79306dae33a9859dbda44e5ef62d66b4da
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-20 02:34:20 -0700

    sidebar toggle setup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,18 @@ import React from "react"
 import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
-const Sidebar = () => {
-  return <h2>sidebar component</h2>
+import { Link } from "gatsby"
+const Sidebar = ({ isOpen, toggleSidebar }) => {
+
+  return <aside className={`sidebar ${isOpen ? "show-sidebar" : ''}`}>
+    <button className="close-btn">
+      <FaTimes />
+    </button>
+    <div className="side-container">
+      <Links styleClass="sidebar-links" />
+      <SocialLinks styleClass="sidebase-icons" />
+    </div>
+  </aside>
 }
 
 export default Sidebar
