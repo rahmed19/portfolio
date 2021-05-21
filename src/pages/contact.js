bba646fac4036ae1e35406edbd0fef70d1bb1c84
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-20 19:42:50 -0700

    google nodemailer

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,23 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return <Layout>
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>Get in touch</h3>
+        <form>
+          <div className="form-group">
+            <input type="text" placeholder="name" className="form-control" />
+            <input type="email" placeholder="email" className="form-control" />
+            <textarea className="form-control" name="message" rows="5" placeholder="message" />
+          </div>
+          <button type="submit" className="submit-btn btn">
+            submit here
+          </button>
+        </form>
+      </article>
+    </section>
+  </Layout>
 }
 
 export default contact
