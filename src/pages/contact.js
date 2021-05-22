7694299819d05029e1a18c7e66870344605dab82
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-21 17:51:02 -0700

    Email JS setup

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,31 @@
 import React from "react"
 import Layout from "../components/Layout"
+import emailjs from 'emailjs-com'
+
+function sendEmail(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('gmail', 'template_8l8xnsk', e.target, process.env.REACT_API_EMAILJS)
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+  console.log("should have been sent")
+}
+
+
 
 const contact = () => {
   return <Layout>
     <section className="contact-page">
       <article className="contact-form">
         <h3>Get in touch</h3>
-        <form>
+        <form onSubmit={sendEmail}>
           <div className="form-group">
-            <input type="text" placeholder="name" className="form-control" />
-            <input type="email" placeholder="email" className="form-control" />
-            <textarea className="form-control" name="message" rows="5" placeholder="message" />
+            <input type="text" placeholder="name" className="form-control" name="from_name" />
+            <input type="email" placeholder="email" className="form-control" name="from_email" />
+            <textarea className="form-control" rows="5" placeholder="message" name="message" />
           </div>
           <button type="submit" className="submit-btn btn">
             submit here
